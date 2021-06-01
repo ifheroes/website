52659bc3cb8c27fcd1abf5c11fd939c712058970
chrischivlog <chrischivlog@gmail.com>
2021-06-01 22:32:50 +0200

    Added Box
    + Box für Startseite hinzugefügt

--- index.php
+++ index.php
@@ -4,7 +4,25 @@
     </head>
     
     <body>
-        <div id="rectangle">
+
+    <div class="box-bigmenu">
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+    </div>
+    <div class="box">
+        <div>
+            <div class="yellow-box">
+                <left>
+                    <h1>HALLO</h1>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+                </left>
+            </div>
         </div>
+        <div>
+            <div class="yellow-image" style="background:url('https://media.discordapp.net/attachments/429020424630829076/784491359272435732/2020-11-21_21.58.50.png') no-repeat center center;">
+            </div>
+        </div>
+    </div>
     </body>
 </html>
