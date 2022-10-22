a2bcdd89d2cda38008665cebe44b2de042a3ea09
chrischivlog <coneisz@chrischi.one>
2022-10-22 13:17:26 +0200

    Update games-faq-class.php

--- pages/games/games-faq-class.php
+++ pages/games/games-faq-class.php
@@ -1,3 +1,7 @@
+<center>
+<h4 id="<?php echo $data_lang->games[0]->survival_title; ?>"><?php echo $data_lang->games[0]->survival_title; ?></h4>
+</center>
+
 <?php
 
 // get content form json file content/job_info.json via system/json_files.php 
@@ -5,39 +9,10 @@ $data = file_get_contents($json['games_faq'], true);
 // JSON decode
 $obj = json_decode($data);
 
+$count = count($obj->survival);
 
-///count entries of file
-$count = count($obj);
+echo $obj->survival[1]->title;
+echo $count;
 
-?>
-
-<center>
-<h4 id="<?php echo $data_lang->games[0]->survival_title; ?>"><?php echo $data_lang->games[0]->survival_title; ?></h4>
-</center>
-
-
-<div class="container">
-    <div class="row">
-        <div class="col-sm">
-        <?php
-        foreach ($obj as $mydata) ///Get data foreach entry in lines etc.
-        {
-        ?>
-        <p>
-            <details>
-                <summary>
-                <b><?php echo $mydata->title; ?></b>
-                </summary>
-                <div class="sum-word-breakUI">
-                    <p>
-                    <?php echo "<br>" . $mydata->text; ?>
-                    </p>
-                </div>
-            </details>
-        </p>
-            <?php
-        }
-        ?>
-        </div>
-    </div>
-</div>
+#start at 0 count up until the number of count is reached
+#display all content in faq
