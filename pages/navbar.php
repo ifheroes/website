0e387683e46a0899dbeb9a244ab65e49cec2339f
chrischivlog <coneisz@chrischi.one>
2022-11-12 17:53:23 +0100

    Google Fonts entfernt
    - Google fonts wurde in diesem Commit aus der Webseite entfernt.
    + Fonts Lokal in Ordner
    + Fonts werden nun über eine extra css Klasse importiert.

--- pages/navbar.php
+++ pages/navbar.php
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light ">
         <a class="navbar-brand" href="index.php">
             <img class="avatar" src="assets/img/infh-astro-blue.png">
-            <text class="color-font" style="font-family: 'NTR' ;margin-left: 7px; color:#333;"><?php echo $data_lang->navbar[0]->title;?></text>
+            <text class="title_ifheroes"><?php echo $data_lang->navbar[0]->title;?></text>
 
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
